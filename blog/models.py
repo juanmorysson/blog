534062a423d9e463131097fb353923200feeea0e
Juan Morysson <juanmorysson@gmail.com>
2020-01-15 15:00:08 -0300

    css

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,9 @@ from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    
+    photo = models.ImageField(upload_to='images/', null=True, blank=True)
+    
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
